8991802b8271894cadee16fb6f2eb7979bb6a047
Serhii Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-04-08 11:14:24 +0300

    task-1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ import {
   ForbesList,
   CryptoHistory,
 } from 'components';
-
+import dataArticle from 'data/article.json';
 export const App = () => {
   return (
     <Section>
@@ -15,7 +15,15 @@ export const App = () => {
         <Heading marginBottom="50px" textAlign="center">
           Task 1
         </Heading>
-        <BlogCard />
+        <BlogCard
+          poster={dataArticle.poster}
+          tag={dataArticle.tag}
+          title={dataArticle.title}
+          description={dataArticle.description}
+          name={dataArticle.name}
+          avatar={dataArticle.avatar}
+          postedAt={dataArticle.postedAt}
+        />
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 2
--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -14,6 +14,35 @@ import {
   Date,
 } from './BlogCard.styled';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import { formatDateToNow } from 'helpers/formatDateToNow';
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  name,
+  avatar,
+  postedAt,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={name} />
+          <UserInfo>
+            <UserName>{name}</UserName>
+            <Date>{formatDateToNow(postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
